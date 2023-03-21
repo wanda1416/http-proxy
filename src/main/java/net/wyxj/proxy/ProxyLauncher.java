5cd757a622f6ef0113bd90ecb724f8879c027346
hui.wang <hui.wang@kyligence.io>
2023-03-21 10:29:30 +0800

    add

--- src/main/java/net/wyxj/proxy/ProxyLauncher.java
+++ src/main/java/net/wyxj/proxy/ProxyLauncher.java
@@ -1,19 +1,24 @@
 package net.wyxj.proxy;
 
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
+import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 
 @RestController
@@ -24,6 +29,15 @@ public class ProxyLauncher {
 
     @Value("${forward.url}")
     private String forwardUrl;
+
+    @Value("${authorization}")
+    private boolean authorization;
+
+    @Value("${username}")
+    private String username;
+
+    @Value("${password}")
+    private String password;
 
     @PostConstruct
     public void init() {
@@ -39,10 +53,21 @@ public class ProxyLauncher {
         }
         RestTemplate restTemplate = new RestTemplate();
         HttpLogUtils.log(request, httpEntity.getBody());
-        ResponseEntity<String> response = restTemplate.exchange(
-                url,
-                Objects.requireNonNull(HttpMethod.resolve(method)),
-                httpEntity, String.class);
+        if (authorization) {
+            byte[] auth = Base64.encodeBase64((username + ":" + password).getBytes());
+            HttpHeaders newHeaders = HttpHeaders.writableHttpHeaders(httpEntity.getHeaders());
+            newHeaders.add("Authorization", "Basic " + new String(auth, StandardCharsets.UTF_8));
+            httpEntity = new HttpEntity<>(httpEntity.getBody(), newHeaders);
+        }
+        ResponseEntity<String> response;
+        try {
+            response = restTemplate.exchange(
+                    url,
+                    Objects.requireNonNull(HttpMethod.resolve(method)),
+                    httpEntity, String.class);
+        } catch (HttpClientErrorException | HttpServerErrorException e) {
+            response = new ResponseEntity<>(e.getResponseBodyAsString(), e.getResponseHeaders(), e.getStatusCode());
+        }
         HttpLogUtils.log(response, response.getBody());
         return response;
     }
